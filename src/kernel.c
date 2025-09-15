9774e45a8d2d445ae938cbb14204330e4401b9ca
kaveO <alban.billiette@gmail.com>
2025-09-15 22:37:40 +0200

    + | Mandatory part finished -> going for bonuses & added .vscode in .gitignore

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,19 @@
+#include "kernel.h"
+
 void kmain(void);
+
+void WriteString(int colour, const char *string)
+{
+	char *video = (char *)0xB8000;
+
+	while(*string)
+	{
+		*video++ = *string++;
+		*video++ = colour;
+	}
+}
 
 void kmain(void)
 {
-
+	WriteString(VGA_PURPLE, "42");
 }

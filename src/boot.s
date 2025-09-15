8ba3fa34f07af20e2e336466ab7b1aa0f55d71e1
kaveO <alban.billiette@gmail.com>
2025-09-15 03:57:24 +0200

    + | Mandatory src files

--- src/boot.s
+++ src/boot.s
@@ -1,5 +1,25 @@
 BITS 32
 
 section .text
-	DD 0x1BADB002 ; Grub magic number https://wiki.osdev.org/Multiboot
-	
+	ALIGN 4							; Next -> multiple of 4 bytes
+	dd 0x1BADB002 					; Grub magic number https://wiki.osdev.org/Multiboot
+	dd 0x00000000
+	dd -(0x1BADB002 + 0x00000000) 	; Checksum
+
+global start
+extern kmain						; Kernel Main
+
+start:
+	cli
+	mov esp, stack_space
+	call kmain
+	hlt
+
+HalthKernel:
+	cli
+	hlt
+	jmp HalthKernel
+
+section .bss
+resb 8192
+stack_space:

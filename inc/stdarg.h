ce3f37eaf2217e1e19c31d8463a8a774a7a44f85
kaveOO <alban.billiette@gmail.com>
2025-10-22 16:11:22 +0100

    + | Implementation of stdarg.h

---
+++ inc/stdarg.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// https://stackoverflow.com/questions/32291808/how-to-implement-stdarg-in-c
+
+/*
+	* Copyright (c) 1992 David I. Bell
+	* Permission is granted to use, distribute, or modify this source,
+	* provided that this copyright notice remains intact.
+
+	* SIMULATE_STDARG
+	*
+	* WARNING: This type of stdarg makes assumptions about the stack
+	*             that may not be true on your system.  You may want to
+	*            define STDARG (if using ANSI C) or VARARGS.
+*/
+
+typedef char 	*va_list;
+
+#define 		va_start(ap,parmn)	(void)((ap) = (char*)(&(parmn) + 1))
+#define 		va_end(ap) 			(void)((ap) = 0)
+#define 		va_arg(ap, type)	(((type*)((ap) = ((ap) + sizeof(type))))[-1])

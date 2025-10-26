4f5fd85d21999b618dedcb673c5eb1b1262bdbe6
kaveOO <alban.billiette@gmail.com>
2025-10-26 18:25:03 +0100

    ~ | Rework of cursor, fixed Wconversion warning & update_cursor() function

--- src/cursor.c
+++ src/cursor.c
@@ -1,13 +1,20 @@
 #include "cursor.h"
-#include "kernel.h"
-#include "types.h"
-#include "io.h"
 
-void UpdateCursor() {
-	uint16_t pos = (video - VGA_ENTRY) / 2; // Calculte bytes from VGA entry
+// Cursor is moving following the video memory
+void move_cursor() {
+	int	pos = ((vga - VGA_ENTRY) / 2); // Calculte bytes from VGA entry
 
 	outb(CURSOR_CMD, 0x0F);
 	outb(CURSOR_DATA, (uint8_t) (pos & 0xFF));
 	outb(CURSOR_CMD, 0x0E);
 	outb(CURSOR_DATA, (uint8_t) ((pos >> 8) & 0xFF));
+}
+
+void update_cursor(int row, int col) {
+	int	pos = ((row * VGA_WIDTH) + col);
+
+	outb(CURSOR_CMD, 0x0F);
+	outb(CURSOR_DATA, (uint8_t)(pos & 0xFF));
+	outb(CURSOR_CMD, 0x0E);
+	outb(CURSOR_DATA, (uint8_t)((pos >> 8) & 0xFF));
 }

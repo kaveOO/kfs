a5b64d1346a3ec21a25678127f141bbcb2b0d8a9
kaveOO <alban.billiette@gmail.com>
2025-11-11 00:45:55 +0100

    + | Added NULL (commented for now)

--- inc/types.h
+++ inc/types.h
@@ -4,6 +4,10 @@
 #define true	1
 #define false	0
 
+// #ifndef NULL
+// #define NULL ((void *)0)
+// #endif
+
 typedef	unsigned char		uint8_t;
 typedef	unsigned short		uint16_t;
 typedef	unsigned int		uint32_t;

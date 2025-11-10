ac2698e44b7d2f8547fe3ba4173b8558e6782832
kaveOO <alban.billiette@gmail.com>
2025-11-11 00:44:11 +0100

    + | %d handling in printk

--- lib/printk.c
+++ lib/printk.c
@@ -1,5 +1,20 @@
 #include "lib.h"
 
+int putnbrk(int nb, unsigned int color) {
+	int count = 0;
+
+	if (nb < 0) {
+		count += writek('-', 1, color);
+		nb = -nb;
+	} else if (nb < 10) {
+		count += writek(nb + '0', 1, color);
+	} else {
+		count += putnbrk(nb / 10, color);
+		count += putnbrk(nb % 10, color);
+	}
+	return count;
+}
+
 int printk(unsigned int color, const char *str, ...) {
 	va_list list;
 	va_start(list, str);
@@ -12,9 +27,12 @@ int printk(unsigned int color, const char *str, ...) {
 			switch (str[i]) {
 				case 'c':
 					count += putchark(va_arg(list, int), color);
-					break;
+					// Fallthrough
 				case 's':
 					count += putstrk(va_arg(list, const char *), color);
+					// Fallthrough
+				case 'd':
+					count += putnbrk(va_arg(list, int), color);
 			}
 			continue;
 		}

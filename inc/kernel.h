bda69648fd1dd96f87652319279bf77d5994a701
kaveOO <alban.billiette@gmail.com>
2025-11-12 09:22:30 +0100

    + | Gobal defines in kernel.h

--- inc/kernel.h
+++ inc/kernel.h
@@ -1,6 +1,13 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#define	VGA_WIDTH		80
+#define	VGA_HEIGHT		25
+#define	VGA_ENTRY		(unsigned char *) 0xB8000
+#define	VGA_LINE		VGA_WIDTH * 2
+#define	VGA_SIZE		(VGA_WIDTH * VGA_HEIGHT) * 2
+#define	VGA_END			VGA_ENTRY + VGA_SIZE
+
 #include "lib.h"
 #include "vga.h"
 #include "types.h"
@@ -10,5 +17,6 @@
 #include "io.h"
 #include "keyboard.h"
 #include "theme.h"
+#include "screen.h"
 
 #endif

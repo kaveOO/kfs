9c6f605ae1e55515edd66c58284a967a0b6be075
kaveOO <alban.billiette@gmail.com>
2026-03-09 08:51:50 +0100

    ~ | putstrk refacto

--- lib/putstrk.c
+++ lib/putstrk.c
@@ -3,8 +3,6 @@
 int putstrk(const char *str, unsigned int color) {
 	int len = strlenk(str);
 
-	for (int i = 0; i < len; i++) {
-		writek(str[i], 1, color);
-	}
+	for (int i = 0; i < len; i++) writek(str[i], 1, color);
 	return len;
 }

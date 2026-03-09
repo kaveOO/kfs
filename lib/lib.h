b5e7d611f1c05545356aa13bbf46bf6527d02e9e
kaveOO <alban.billiette@gmail.com>
2026-03-09 11:20:37 +0100

    + | Added strcmpk and refacto of the lib.h file

--- lib/lib.h
+++ lib/lib.h
@@ -3,12 +3,13 @@
 
 #include "kernel.h"
 
-int putchark(int c, unsigned int color);
-int putstrk(const char *str, unsigned int color);
-int strlenk(const char *str);
-int writek(int c, int len, unsigned int color);
-int printk(unsigned int color, const char *str, ...);
-int	strncmpk(const char *s1, const char *s2, unsigned int n);
-void *memsetk(void *ptr, int value, unsigned long num);
+int		putchark(int c, unsigned int color);
+int		putstrk(const char *str, unsigned int color);
+int		strlenk(const char *str);
+int		writek(int c, int len, unsigned int color);
+int		printk(unsigned int color, const char *str, ...);
+int		strncmpk(const char *s1, const char *s2, unsigned int n);
+void	*memsetk(void *ptr, int value, unsigned long num);
+int		strcmpk(const char *s1, const char *s2);
 
 #endif

4b4e2857ead9fa1d495cdde65ab0b42bc9045e85
kaveOO <alban.billiette@gmail.com>
2026-03-09 08:49:15 +0100

    + | strncmpk function

--- lib/strncmpk.c
+++ lib/strncmpk.c
@@ -1,19 +1,13 @@
 #include "lib.h"
 
-int	strncmpk(const char *s1, const char *s2, int n) {
-	int	i;
+int	strncmpk(const char *s1, const char *s2, unsigned int n) {
+	unsigned int i = 0;
 
-	if (n == 0)
-		return (0);
-
-	i = 0;
+	if (n == 0) return (0);
 
 	while (s1[i] == s2[i] && s1[i] != '\0') {
-		if (i < (n - 1))
-			i++;
-		else
-			return (0);
+		if (i < (n - 1)) i++;
+		else return (0);
 	}
-
 	return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
 }

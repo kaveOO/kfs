51e100c9f26cfca8d8ead4b75b1eafabb0f5a7ee
kaveOO <alban.billiette@gmail.com>
2026-03-09 08:53:08 +0100

    ~ | Refacto of memsetk

--- lib/memsetk.c
+++ lib/memsetk.c
@@ -3,9 +3,6 @@
 void *memsetk(void *ptr, int value, unsigned long num) {
 	unsigned char *p = (unsigned char *)ptr;
 
-	while (num--) {
-		*p++ = (unsigned char)value;
-	}
-
+	while (num--) *p++ = (unsigned char)value;
 	return ptr;
 }
